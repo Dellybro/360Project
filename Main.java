beb7b39016eb04365f770b809fc4045496ef72f8
FugenApps <Dellybro@traviss-mbp.mobile.asu.edu>
2017-10-22 10:51:38 -0700

    All files created

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+   public static Scanner input = new Scanner(System.in);
+
+
+   public static void main(String[] argv){
+
+   }
+
+   public static void showMenu(){
+      /* Add gui stuff here*/
+   }
+}
