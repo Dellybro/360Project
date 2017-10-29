0f2c4963de8495176b9d699b43c8a26beb316908
Brandon Quan <bq2398@gmail.com>
2017-10-29 16:47:33 -0700

    Added remove punctuation
    Added remove punctuation and made methods static

--- FileIO.java
+++ FileIO.java
@@ -1,21 +1,19 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class FileIO {
 	
-	private String f;
 	
-	public FileIO(String name){
-		f = name;
-	}
-	
-	public FileAnalyzer readFile() {
-		PriorityQueue<WordTotal> commonWords = new PriorityQueue<WordTotal>(); //Used to keep track of the most used words
+	public static FileAnalyzer readFile(String name) {
+		PriorityQueue<WordTotal> commonWords = new PriorityQueue<WordTotal>();
 		int numOfLines = 0; //Used to keep track of the number of lines
 		int numOfBlankLines = 0;//Used to keep track of the number of blank lines
 		int numOfSpaces = 0;//Used to keep track of the number of spaces
@@ -23,30 +21,31 @@ public class FileIO {
 		int averageCharsPerLine = 0; //Used to keep a char count
 		int averageWordLength = 0; //Used to keep a count of the word lengths
 		try {
-			BufferedReader bf = new BufferedReader(new FileReader(new File(f)));//Used to read the file
-			String line;//Current line in the file
-			HashMap<String, Integer> wordCount = new HashMap<String, Integer>();//Keeps track of the word with its' count
-			while((line = bf.readLine()) != null){//Reads in the current line
+			BufferedReader bf = new BufferedReader(new FileReader(new File(name)));
+			String line;
+			//FileAnalyzer stats = new FileAnalyzer();
+			HashMap<String, Integer> wordCount = new HashMap<String, Integer>();//add words
+			while((line = bf.readLine()) != null){
 				if(line.length() != 0){//May need additional checks for new line
 					averageCharsPerLine += line.length();//Adding the char count
 					int leftP = -1;
 					for(int i = 0; i < line.length(); i++){
-						if(line.charAt(i) == ' '){//Check if the current char is a space
-							numOfSpaces++;//Increment the aount of spaces
+						if(line.charAt(i) == ' '){
+							numOfSpaces++;
 						}
-						if(leftP == -1 && Character.isLetter(line.charAt(i))){//Start the current word
+						if(leftP == -1 && Character.isLetter(line.charAt(i))){
 							leftP = i;
 						}else if((!Character.isLetter(line.charAt(i))|| i == line.length()-1) && leftP != -1){//reached end of word, add to HashMap
 							String toAdd;
 							if(i == line.length()-1 && Character.isLetter(line.charAt(i))){
-								toAdd = line.substring(leftP, i+1).toLowerCase(); //Case that the end of line ends the word
+								toAdd = line.substring(leftP, i+1).toLowerCase(); //Case that word ends the line
 							}else{
-								toAdd = line.substring(leftP, i).toLowerCase(); //Case that space ends the word
+								toAdd = line.substring(leftP, i).toLowerCase(); //Case that space ends the line
 							}
 							numOfWords++;//Keeps track of how many words in the text file
 							averageWordLength += toAdd.length(); //Adding the word length
-							if(wordCount.containsKey(toAdd)){//Check if the word has been seen before
-								wordCount.put(toAdd, wordCount.get(toAdd) + 1);//Increment the word's count
+							if(wordCount.containsKey(toAdd)){
+								wordCount.put(toAdd, wordCount.get(toAdd) + 1);
 							}else{
 								wordCount.put(toAdd, 1);//Word only appears once
 							}
@@ -65,9 +64,9 @@ public class FileIO {
 			if(numOfWords != 0){//Divide by 0 check
 				averageWordLength /= numOfWords; //Averaging the word length
 			}
-			for(String s: wordCount.keySet()){//Move the words from a map to a priority queue
-				WordTotal w = new WordTotal(s, wordCount.get(s));//Class which contains a word and its' count
-				commonWords.offer(w);//Adding the word and count to the priority Queue
+			for(String s: wordCount.keySet()){
+				WordTotal w = new WordTotal(s, wordCount.get(s));
+				commonWords.offer(w);
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File was invalid/not found");
@@ -76,10 +75,37 @@ public class FileIO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Returning a fileanalyzer
-		return new FileAnalyzer(numOfLines, numOfBlankLines, numOfSpaces, numOfWords, averageCharsPerLine, averageWordLength, commonWords, f);
+		return new FileAnalyzer(numOfLines, numOfBlankLines, numOfSpaces, numOfWords, averageCharsPerLine, averageWordLength, commonWords, name);
 	}
 	
+	public static File removePunctuation(String name){
+		File nf = new File("test.txt");
+		BufferedReader bf;
+		PrintWriter pw;
+		try {
+			bf = new BufferedReader(new FileReader(new File(name)));
+			String newFile = "test.txt";
+			pw = new PrintWriter("test"+".txt");
+			String line;
+			while((line = bf.readLine()) != null){
+				for(int i = 0; i < line.length(); i++){
+					char c = line.charAt(i);
+				    if(c != '!' && c != '.' && c != '?'  && c != ';' && c != ':' && c != ',' && c != 39){//39 is a single quote
+						pw.print(c);
+				    }
+				}
+				pw.println();
+			}
+			pw.close();
+			bf.close();
+		} catch (FileNotFoundException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		} catch (IOException ex){
+			ex.printStackTrace();
+		}
+		return nf;
+	}
 	
 }
 
